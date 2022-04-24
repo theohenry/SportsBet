2642c00143c67bd6b86df5f6ef3be6fd75622900
kunalvalia <ikunalvalia@gmail.com>
2022-04-24 12:20:32 -0400

    build json file complete

---
+++ buildJson.py
@@ -0,0 +1,82 @@
+# buildJson.py
+# Theo Henry, Kunal Valia, Gustavo Curioso
+# April 2022
+# Read from a spreadsheet of team data and output a JSON file containing 
+# each teams probabilities of an event occuring in a possesion 
+
+import json
+import pandas as pd
+
+# Given a dictionary of season data, calculate and return a dictionary
+# containing the probabilities of each outcome on any given possession
+def get_team_probabilities(teamParse):
+    two_make_total = 0
+    two_miss_total = 0
+    three_make_total = 0
+    three_miss_total = 0
+    turnover_total = 0
+    fta_total = 0
+    possession_total = 0
+    oreb_total = 0
+    dreb_total = 0
+    rebound_total = 0
+    ft_made_total = 0
+    ft_miss_total = 0
+
+    # Loop through each game in a season. Get totals for each stat
+    for game_num in teamParse['GAME_ID'].keys():
+        game_misses = teamParse["FG2A"][game_num] - teamParse["FG2M"][game_num] + teamParse["FG3A"][game_num] - teamParse["FG3M"][game_num]
+        two_make_total += teamParse["FG2M"][game_num]
+        two_miss_total += teamParse["FG2A"][game_num] - teamParse["FG2M"][game_num]
+        three_make_total += teamParse["FG3M"][game_num]
+        three_miss_total += teamParse["FG3A"][game_num] - teamParse["FG3M"][game_num]
+        turnover_total += teamParse["TOV"][game_num]
+        oreb_total += teamParse["OREB"][game_num]
+        dreb_total += game_misses - teamParse["OREB"][game_num]
+        fta_total += teamParse["FTA"][game_num]
+        ft_made_total += teamParse["FTM"][game_num]
+        ft_miss_total += teamParse["FTA"][game_num] - teamParse["FTM"][game_num]
+        
+    # Compute the number of shooting fouls based on FTA
+    # NBA research shows 44% of free throws attempted equals half the number of fouls 
+    shooting_fouls_total = fta_total * 0.44
+    # Get total number of possessions for the team in the entire season
+    possession_total = two_make_total + two_miss_total + three_make_total + three_miss_total + turnover_total + shooting_fouls_total
+    rebound_total = oreb_total + dreb_total
+
+    teamDict = {}
+    teamDict['2MAKE'] = two_make_total / possession_total
+    teamDict['2MISS'] = two_miss_total / possession_total
+    teamDict['3MAKE'] = three_make_total / possession_total
+    teamDict['3MISS'] = three_miss_total / possession_total
+    teamDict['TO'] = turnover_total / possession_total
+    teamDict['FOUL'] = shooting_fouls_total / possession_total
+    teamDict['OREB'] = oreb_total / rebound_total
+    teamDict['DREB'] = dreb_total / rebound_total
+    teamDict['FTMA'] = ft_made_total / fta_total
+    teamDict['FTMI'] = ft_miss_total / fta_total
+    teamDict['POS_PG'] = round((possession_total - oreb_total) / len(teamParse['GAME_ID'].keys()))
+
+    return teamDict
+
+# Read from an existing spreadsheet of team data and build a json file to 
+# output every teams probabilities
+def build_json():
+    xls = "GameData.xlsx"
+    f = pd.ExcelFile(xls)
+    sheet_names = f.sheet_names
+    nba_dict = {}
+
+    # Loop for every team
+    for i in range(len(sheet_names)):
+        team_name = sheet_names[i]
+        data = pd.read_excel(xls, sheet_names)
+        teamJson = data[team_name].to_json()
+        teamParse = json.loads(teamJson)
+        nba_dict[team_name] = get_team_probabilities(teamParse)
+
+    # Write data to the json file probability.json
+    with open('probability.json', 'w') as f:
+        json.dump(nba_dict, f, indent=2)
+
+build_json()
--- writeGames.py
+++ writeGames.py
@@ -1,3 +1,8 @@
+# writeGames.py
+# Theo Henry, Kunal Valia, Gustavo Curioso
+# April 2022
+# Write NBA statistics for every game for every team to an excel file
+
 from nba_api.stats.static import teams
 from nba_api.stats.endpoints import leaguegamefinder
 from nba_api.stats.library.parameters import Season
@@ -9,14 +14,19 @@ import xlsxwriter
 import numpy as np
 from sklearn.datasets import load_iris
 
+# Logic
 # for each team
 #   add new sheet with team name
 #   add header
 #   for each game
 #      format game data
 #      add game data to table
-headers = ['GAME_ID', 'MATCHUP', 'GAME_ID', 'WL', 'PTS', 'FG2M', 'FG2A', 'FG3M', 'FG3A', 'FTM', 'FTA', 'OREB', 'DREB', 'STL', 'BLK', 'TOV', 'PF']
 
+# Headers for excel file
+headers = ['GAME_ID', 'MATCHUP', 'GAME_DATE', 'WL', 'PTS', 'FG2M', 'FG2A', 'FG3M', 'FG3A', 'FTM', 'FTA', 'OREB', 'DREB', 'STL', 'BLK', 'TOV', 'PF']
+
+# Takes as argument a dictionary with game data and returns an array with
+# each header filled
 def get_game_array(game):
     row_array = list()
 
@@ -32,22 +42,25 @@ def get_game_array(game):
         
     return row_array
 
+# Writes game data to excel file for each team
 def write_games_to_file():
     wb = xlsxwriter.Workbook('GameData.xlsx')
+    # Get list of NBA teams
     nba_teams = teams.get_teams()
     for team in nba_teams:
-        # might need team[0]
         name = team['abbreviation']
+        # Initialize new excel sheet with team name and headers
         sheet = wb.add_worksheet(name)
         sheet.write_row(0, 0, headers)
         team_id = team['id']
+        # NBA_API call to get game data for every game thus far
         gamefinder = leaguegamefinder.LeagueGameFinder(team_id_nullable=team_id,
                             season_nullable=Season.default,
                             season_type_nullable=SeasonType.regular)
         games_dict = gamefinder.get_normalized_dict()
         games = games_dict['LeagueGameFinderResults']
+        # Add new row for each game
         for i in range(len(games)):
-            # game_data = get_game_array(games[len(games) - i - 1])
             game_data = get_game_array(games[i])
             sheet.write_row(i+1, 0, game_data)
 
